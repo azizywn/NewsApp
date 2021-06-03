abd2a47cd85e5dbe23f57c8213335f447e972d8c
azizywn <63053486+azizywn@users.noreply.github.com>
2021-06-03 10:57:01 +0700

    Add files via upload

---
+++ NewsApp/NewsApp/Model/News.swift
@@ -0,0 +1,16 @@
+//
+//  News.swift
+//  NewsApp
+//
+//  Created by Hakim on 05/02/21.
+//
+
+import SwiftUI
+
+
+struct News : Identifiable{
+    var id = UUID()
+    var title : String
+    var image : String
+    var description : String
+}
---
+++ NewsApp/NewsApp/ViewModel/NewsApi.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApi.swift
+//  NewsApp
+//
+//  Created by Hakim on 05/02/21.
+//
+
+//pengirim = ObservableObject
+//penerima = ObservedObject
+
+import Foundation
+import Combine
+import SwiftyJSON
+
+class NewsApi: ObservableObject {
+    
+}
